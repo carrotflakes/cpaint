36e5df58057bf845c45a9dde986a7f627601ba66
carrotflakes <carrotflakes@gmail.com>
2024-08-17 03:46:19 +0900

    Fill

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -16,24 +16,40 @@ export default function Canvas() {
     onMouseDown: (pos, e) => {
       let lastPos = pos;
       e.preventDefault();
+      const poss = [pos];
       tmpCanvas.begin({
         size: [store.canvas.width, store.canvas.height],
-        style: store.tool === "pen" ? store.color : "#fff",
+        style: {
+          pen: store.color,
+          eraser: "#fff",
+          fill: "#f00",
+        }[store.tool],
         soft: store.softPen,
         opacity: store.opacity,
       });
       return {
         onMouseMove: (pos) => {
           if (dist(lastPos, pos) > 10) {
+            poss.push(pos);
             tmpCanvas.addLine({
               line: [...lastPos, ...pos],
-              lineWidth: store.penSize,
+              lineWidth: store.tool === "fill" ? 1 : store.penSize,
             });
             lastPos = pos;
             setUpdatedAt(Date.now());
           }
         },
         onMouseUp: () => {
+          if (store.tool === "fill") {
+            const ctx = tmpCanvas.canvas.getContext("2d")!;
+            ctx.fillStyle = store.color;
+            ctx.clearRect(0, 0, store.canvas.width, store.canvas.height);
+            ctx.beginPath();
+            for (const pos of poss) {
+              ctx.lineTo(pos[0], pos[1]);
+            }
+            ctx.fill();
+          }
           store.finish(tmpCanvas.canvas);
           tmpCanvas.finish();
         },
