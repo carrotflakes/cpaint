cbfcf44ae13d19a61dc0beb2ec43af09a5263eb4
carrotflakes <carrotflakes@gmail.com>
2024-08-17 03:50:51 +0900

    Remove unused code

--- src/state.ts
+++ src/state.ts
@@ -1,5 +1,4 @@
 import { create } from 'zustand';
-import { drawSoftLine } from './ccanvas';
 
 type ToolType = "pen" | "eraser" | "fill";
 
@@ -9,10 +8,7 @@ export type State = {
   opacity: number
   softPen: boolean
   canvas: HTMLCanvasElement
-  canvasRaster: HTMLCanvasElement
   imageData: ImageData | null
-  addLine: (line: [number, number, number, number]) => void
-  begin: () => void
   finish: (canvas: HTMLCanvasElement) => void
   setSize: (width: number, height: number) => void
   updatedAt: Date
@@ -32,40 +28,6 @@ export const useStore = create<State>()((set) => ({
   canvas: document.createElement("canvas"),
   canvasRaster: document.createElement("canvas"),
   imageData: null,
-  addLine(line) {
-    set((state) => {
-      if (!state.softPen) {
-        const ctx = state.canvasRaster.getContext("2d")!;
-        ctx.lineCap = "round";
-        ctx.strokeStyle = state.tool === "pen" ? state.color : "#fff";
-        ctx.lineWidth = state.penSize;
-        ctx.beginPath();
-        ctx.moveTo(line[0], line[1]);
-        ctx.lineTo(line[2], line[3]);
-        ctx.stroke();
-      } else {
-        if (state.imageData) {
-          drawSoftLine(state.imageData, line[0], line[1], line[2], line[3], state.penSize);
-          const ctx = state.canvasRaster.getContext("2d")!;
-          ctx.putImageData(state.imageData, 0, 0);
-        }
-      }
-      return { updatedAt: new Date() }
-    })
-  },
-  begin() {
-    set((state) => {
-      const ctx = state.canvasRaster.getContext("2d")!;
-      ctx.fillStyle = state.tool === "pen" ? state.color : "#fff";
-      ctx.fillRect(0, 0, state.canvasRaster.width, state.canvasRaster.height);
-      const imageData = ctx.getImageData(0, 0, state.canvasRaster.width, state.canvasRaster.height);
-      ctx.clearRect(0, 0, state.canvasRaster.width, state.canvasRaster.height);
-      // set to transparent
-      for (let i = 3; i < imageData.data.length; i += 4)
-        imageData.data[i] = 0;
-      return { imageData, updatedAt: new Date() }
-    })
-  },
   finish(canvas) {
     set((state) => {
       const ctx = state.canvas.getContext("2d")!;
@@ -81,8 +43,6 @@ export const useStore = create<State>()((set) => ({
     set((state) => {
       state.canvas.width = width;
       state.canvas.height = height;
-      state.canvasRaster.width = width;
-      state.canvasRaster.height = height;
       return { updatedAt: new Date() }
     })
   },
