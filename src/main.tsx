8e7a8bbfb389e2e796933bf95120a405ca889f59
carrotflakes <carrotflakes@gmail.com>
2024-10-14 06:29:01 +0900

    Remove unused Init component import

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
 import './index.css'
-import { Init } from './components/Init.tsx'
+// import { Init } from './components/Init.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
-      <Init />
+      {/* <Init /> */}
       <App />
     </Suspense>
   </React.StrictMode>,
