055ed7476bbdd998474476aeccbc98fb6d606cbe
carrotflakes <carrotflakes@gmail.com>
2025-05-25 05:19:40 +0900

    Refactor BrushPreview to use device pixel ratio

--- src/components/BrushPreview.tsx
+++ src/components/BrushPreview.tsx
@@ -12,33 +12,39 @@ export function BrushPreview({
     opacity?: number;
   };
 }) {
+  const width = 300;
+  const height = 100;
+  const dpr = window.devicePixelRatio ?? 1;
+
   const canvasRef = useRef<HTMLCanvasElement>(null!);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
+    const cw = canvas.width;
+    const ch = canvas.height;
     ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, cw, ch);
 
     const touch = startTouchBrush({
       brushType,
-      width: 20,
+      width: 20 * dpr,
       color: "black",
       opacity: 1,
       erase: false,
       alphaLock: false,
-      canvasSize: [canvas.width, canvas.height],
+      canvasSize: [cw, ch],
       ...overwriteProps,
     });
 
-    const startX = 20;
-    const endX = 280;
+    const startX = 20 * dpr;
+    const endX = 280 * dpr;
     const n = 100;
     for (let i = 0; i <= n; i++) {
       touch.stroke(
         startX + ((endX - startX) * i) / n,
-        50 + Math.sin((i / n) * 2 * Math.PI) * 20,
+        (height / 2 + Math.sin((i / n) * 2 * Math.PI) * 20) * dpr,
         Math.sin((i / n) * Math.PI)
       );
     }
@@ -46,5 +52,15 @@ export function BrushPreview({
     touch.transfer(ctx);
   }, [brushType, JSON.stringify(overwriteProps)]);
 
-  return <canvas ref={canvasRef} width={300} height={100}></canvas>;
+  return (
+    <canvas
+      ref={canvasRef}
+      width={(width * dpr) | 0}
+      height={(height * dpr) | 0}
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+      }}
+    ></canvas>
+  );
 }
