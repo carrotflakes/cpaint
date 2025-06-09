589735c89d659eecbac424e7bc44decce40c58c5
carrotflakes <carrotflakes@gmail.com>
2025-06-10 06:22:20 +0900

    Tweak buttons in UnsavedChangesDialog

--- src/components/UnsavedChangesDialog.tsx
+++ src/components/UnsavedChangesDialog.tsx
@@ -1,5 +1,5 @@
-import { useUnsavedChangesDialog } from '../store/unsavedChangesDialog';
-import { ModalDialog } from './ModalDialog';
+import { useUnsavedChangesDialog } from "../store/unsavedChangesDialog";
+import { ModalDialog } from "./ModalDialog";
 
 export function UnsavedChangesDialog() {
   const { isOpen, message, onConfirm, onCancel } = useUnsavedChangesDialog();
@@ -10,10 +10,8 @@ export function UnsavedChangesDialog() {
     <ModalDialog>
       <div className="flex flex-col gap-4">
         <div className="text-lg font-bold">Unsaved Changes</div>
-        <div className="text-gray-700 dark:text-gray-300">
-          {message}
-        </div>
-        <div className="flex gap-2 justify-end">
+        <div className="text-gray-700 dark:text-gray-300">{message}</div>
+        <div className="flex gap-2 justify-end flex-wrap">
           <button
             className="px-4 py-2 rounded bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600"
             onClick={onCancel}
